5a575bf5d433ab0dd79864477b5134752e02b88f
Yongce Tu <yongce.tu@gmail.com>
2020-03-26 00:39:29 +0800

    Refactor the build configs to make the modules's source code can be included by other projects

--- appWear/src/main/java/me/ycdev/android/bluetooth/explorer/MainActivity.kt
+++ appWear/src/main/java/me/ycdev/android/bluetooth/explorer/MainActivity.kt
@@ -3,8 +3,6 @@ package me.ycdev.android.bluetooth.explorer
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import android.widget.TextView
-import me.ycdev.android.bluetooth.ble.R.id
-import me.ycdev.android.bluetooth.ble.R.layout
 
 class MainActivity : WearableActivity() {
 
@@ -12,9 +10,9 @@ class MainActivity : WearableActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        mTextView = findViewById(id.text)
+        mTextView = findViewById(R.id.text)
 
         // Enables Always-on
         setAmbientEnabled()
